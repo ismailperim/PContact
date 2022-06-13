e4a6b995b08b20698b6a969bc5ee5029acf88420
İsmail Perim <ismail@perim.net>
2022-06-13 15:01:42 +0300

    report status update step added

--- src/services/Report/Report.Service/ReportQueueService.cs
+++ src/services/Report/Report.Service/ReportQueueService.cs
@@ -1,11 +1,13 @@
 ﻿using Core.Models;
 using Core.Services.Interfaces;
 using EasyNetQ;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
 using Report.Models;
+using Report.Service.Interfaces;
 
 namespace Report.Service
 {
@@ -13,13 +15,18 @@ namespace Report.Service
     {
         public readonly IBus _rabbitMQBus;
         public readonly IApiClientService _apiClient;
+        public readonly IReportService _service;
 
 
-        public ReportQueueService(IOptions<ServiceOptions> options, IApiClientService apiClient)
+        public ReportQueueService(IServiceProvider provider)
         {
+
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            
-            _apiClient = apiClient;
+
+            var options = provider.GetService<IOptions<ServiceOptions>>();
+
+            _apiClient = provider.GetService<IApiClientService>();
+            _service = provider.GetService<IReportService>();
 
             _rabbitMQBus = RabbitHutch.CreateBus(options.Value.MessageQueueOptions?.ConnectionString);
             _rabbitMQBus.Advanced.QueueDeclare(Constant.Q_REPORT);
@@ -28,42 +35,45 @@ namespace Report.Service
         {
             _rabbitMQBus.SendReceive.ReceiveAsync<Models.Report>(Constant.Q_REPORT, async (x) =>
             {
-                var result = await _apiClient.Get<ContactReport>("v1/report", $"location={x.Location}");
-
-                using (var package = new ExcelPackage())
+                if (x.ID != null)
                 {
-                    var sheet = package.Workbook.Worksheets.Add("ContactReport");
+                    var result = await _apiClient.Get<ContactReport>("v1/report", $"location={x.Location}");
 
-                    var columns = new List<string>() { "Location", "PersonCount", "PhoneCount" };
-                    int currentRow = 1;
-                    
-                    for (int i = 0; i < columns.Count; i++)
+                    using (var package = new ExcelPackage())
                     {
-                        var currentColumn = i + 1;
-                        sheet.Cells[currentRow, currentColumn].Value = columns[i];
-                        sheet.Cells[currentRow, currentColumn].Style.Font.Bold = true;
-                        sheet.Cells[currentRow, currentColumn].Style.Fill.PatternType = ExcelFillStyle.Solid;
-                        sheet.Cells[currentRow, currentColumn].Style.Fill.BackgroundColor.SetColor(1, 0, 88, 239);
-                        sheet.Cells[currentRow, currentColumn].Style.Font.Color.SetColor(1, 255, 240, 239);
+                        var sheet = package.Workbook.Worksheets.Add("ContactReport");
+
+                        var columns = new List<string>() { "Location", "PersonCount", "PhoneCount" };
+                        int currentRow = 1;
+
+                        for (int i = 0; i < columns.Count; i++)
+                        {
+                            var currentColumn = i + 1;
+                            sheet.Cells[currentRow, currentColumn].Value = columns[i];
+                            sheet.Cells[currentRow, currentColumn].Style.Font.Bold = true;
+                            sheet.Cells[currentRow, currentColumn].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                            sheet.Cells[currentRow, currentColumn].Style.Fill.BackgroundColor.SetColor(1, 0, 88, 239);
+                            sheet.Cells[currentRow, currentColumn].Style.Font.Color.SetColor(1, 255, 240, 239);
+                        }
+
+                        currentRow++;
+
+                        sheet.Cells[currentRow, 1].Value = result.Location;
+                        sheet.Cells[currentRow, 2].Value = result.PersonCount;
+                        sheet.Cells[currentRow, 3].Value = result.PhoneCount;
+
+
+                        sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
+                        sheet.Cells[sheet.Dimension.Address].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[sheet.Dimension.Address].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[sheet.Dimension.Address].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                        sheet.Cells[sheet.Dimension.Address].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+                        await package.SaveAsAsync(new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}/wwwroot/upload/{x.ID}.xlsx"));
+
+                        _service.UpdateReport(x.ID.Value, $"/upload/{x.ID}.xlsx");
                     }
-
-                    currentRow++;
-
-                    sheet.Cells[currentRow, 1].Value = result.Location;
-                    sheet.Cells[currentRow, 2].Value = result.PersonCount;
-                    sheet.Cells[currentRow, 3].Value = result.PhoneCount;
-
-
-                    sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
-                    sheet.Cells[sheet.Dimension.Address].Style.Border.Top.Style = ExcelBorderStyle.Thin;
-                    sheet.Cells[sheet.Dimension.Address].Style.Border.Left.Style = ExcelBorderStyle.Thin;
-                    sheet.Cells[sheet.Dimension.Address].Style.Border.Right.Style = ExcelBorderStyle.Thin;
-                    sheet.Cells[sheet.Dimension.Address].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
-
-                    await package.SaveAsAsync(new FileInfo($"{AppDomain.CurrentDomain.BaseDirectory}/wwwroot/upload/{x.ID}.xlsx"));
                 }
-
-
             });
 
             return Task.CompletedTask;
